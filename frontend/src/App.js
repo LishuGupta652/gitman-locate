1f32c8ebdd78c579497c372e169f0d52867e3df6
lishu <lishugupta652@gmail.com>
2021-12-17 00:12:46 +0530

    Frontend : react-map-gl

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import "./App.css";
 
+import { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
 function App() {
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8,
+  });
+
   return (
     <div className="App">
-      <h1>main page</h1>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX}
+        onViewportChange={(nextViewport) => setViewport(nextViewport)}
+      />
     </div>
   );
 }
