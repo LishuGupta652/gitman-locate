0b99f5912351471f9f15631bec35ae2928b85f4b
lishu <lishugupta652@gmail.com>
2021-12-16 18:45:33 +0530

    Gitman Locate: Models

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      min: 3,
+      max: 20,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      min: 6,
+    },
+  },
+  { timestamps: true }
+);
