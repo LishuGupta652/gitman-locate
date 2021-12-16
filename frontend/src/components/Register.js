0f8bc7ca88052a3b46afdce71ed4b0e5c0466d47
lishu <lishugupta652@gmail.com>
2021-12-17 03:12:17 +0530

    Frontend : Auth

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -4,13 +4,15 @@ const Register = () => {
   return (
     <div className="registerContainer">
       <div className="logo">
-        <Room />
+        <Room /> Gitman Locate
       </div>
       <form action="">
         <input type="text" placeholder="username" />
         <input type="email" placeholder="email" />
         <input type="password" placeholder="password" />
-        <button>Register</button>
+        <button className="registerButton">Register</button>
+        <span className="success">Successfull. You can login now</span>
+        <span className="error">Something went wrong</span>
       </form>
     </div>
   );
