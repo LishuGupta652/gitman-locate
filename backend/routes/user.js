dde5778660e6128f0ac07301e4ffa5b32de2057d
lishu <lishugupta652@gmail.com>
2021-12-16 19:19:27 +0530

    Gitman Locate: Routes > User : Register

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/User");
-//
+
+// register User
+router.post("/register", async (req, res) => {
+  try {
+    // Generate a new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    // Create a new User
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    // Save user and send response
+  } catch (err) {
+    return res.status(500).send({ err: err.message });
+  }
+});
+// Login User
 
 module.exports = router;
