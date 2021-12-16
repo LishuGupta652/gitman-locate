81fc378920547a2b0bed3cc62d0da83cc7d45e6b
lishu <lishugupta652@gmail.com>
2021-12-16 18:52:39 +0530

    Gitman Locate: Routes > Pin

--- backend/routes/pin.js
+++ backend/routes/pin.js
@@ -4,6 +4,15 @@ const Pin = require("../models/Pin");
 // Create a pin
 router.post("/", async (req, res) => {
   const newPin = new Pin(req.body);
+
+  try {
+    const savedPin = await newPin.save();
+    return res
+      .status(200)
+      .json({ message: "Pin created successfully", pin: savedPin });
+  } catch (err) {
+    return res.status(500).send({ err: err.message });
+  }
 });
 
 // Get a pin
