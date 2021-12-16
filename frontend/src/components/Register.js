cbd80c8abab19c2f584213a14ecff4ec6b39bed5
lishu <lishugupta652@gmail.com>
2021-12-17 03:09:43 +0530

    Frontend : Auth

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,14 +1,17 @@
+import { Room } from "@mui/icons-material";
+
 const Register = () => {
   return (
     <div className="registerContainer">
       <div className="logo">
-        <form action="">
-          <input type="text" placeholder="username" />
-          <input type="email" placeholder="email" />
-          <input type="password" placeholder="password" />
-          <button>Register</button>
-        </form>
+        <Room />
       </div>
+      <form action="">
+        <input type="text" placeholder="username" />
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="password" />
+        <button>Register</button>
+      </form>
     </div>
   );
 };
