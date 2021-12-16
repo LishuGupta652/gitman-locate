faef4768b70bdb6f41b01d411f9144d561a3ed78
lishu <lishugupta652@gmail.com>
2021-12-17 03:14:53 +0530

    Frontend : Auth

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,18 +1,22 @@
 import { Room } from "@mui/icons-material";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 const Register = () => {
   const [success, setSuccess] = useState(false);
   const [error, setError] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
   return (
     <div className="registerContainer">
       <div className="logo">
         <Room /> Gitman Locate
       </div>
       <form action="">
-        <input type="text" placeholder="username" />
-        <input type="email" placeholder="email" />
-        <input type="password" placeholder="password" />
+        <input type="text" placeholder="username" ref={nameRef} />
+        <input type="email" placeholder="email" ref={emailRef} />
+        <input type="password" placeholder="password" ref={passwordRef} />
         <button className="registerButton">Register</button>
         {success && (
           <span className="success">Successfull. You can login now</span>
